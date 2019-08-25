78b0120c0cb5739e02e8d2e2c57b2e4834524ef8
clauciof <claucio2@gmail.com>
2019-08-25 14:10:29 -0300

    dandjnda

---
+++ Ordenacao/ordenacao.py
@@ -0,0 +1,16 @@
+def insertion_sort(palavra):
+    print(palavra)
+
+def selection_sort():
+    return
+
+def bubble_sort():
+    return 
+
+
+def main():
+      
+    insertion_sort("oi")
+
+if __name__ == "__main__":
+    main()
