ff4e7b6cc8f47d7853cbb0dbc4b9a9c27b7967b0
clauciof <claucio2@gmail.com>
2019-08-31 12:58:44 -0300

    bubble_sort()

--- Ordenacao/ordenacao.py
+++ Ordenacao/ordenacao.py
@@ -1,13 +1,14 @@
-import numpy as np
+
 import matplotlib.pyplot as plt
 import time as tm
 import random
 
-def insertion_sort(vetor, tamanho, y):
+def insertion_sort(vetor, tamanho, x):
     plt.show()
-    for i in range(1, tamanho):   
+    for i in range(1, tamanho): 
+        plt.title("Insertion Sort")  
         plt.grid(True)
-        plt.plot(y,vetor, 'ro')
+        plt.plot(x,vetor, 'ro')
         plt.pause(0.001)
         plt.clf()
         if(vetor[i]<vetor[i-1]):
@@ -17,19 +18,21 @@ def insertion_sort(vetor, tamanho, y):
                 vetor[j] = vetor[j-1]
                 vetor[j-1] = aux
                 j = j - 1
-    
+   
+    plt.title("Insertion Sort") 
     plt.grid(True)
-    plt.plot(y,vetor, 'ro')
+    plt.plot(x,vetor, 'ro')
     plt.show()
     
         
 
-def selection_sort(vetor, tamanho, y):
+def selection_sort(vetor, tamanho, x):
     plt.show()
     for i in range(0, tamanho):
         #gráfico
+        plt.title("Selection Sort")
         plt.grid(True)
-        plt.plot(y,vetor, 'ro')
+        plt.plot(x,vetor, 'ro')
         plt.pause(0.001)
         plt.clf()
         menor = vetor[i]
@@ -42,25 +45,47 @@ def selection_sort(vetor, tamanho, y):
         vetor[indice_menor] = aux
     
     #plota gráfico
+    plt.title("Selection Sort")
     plt.grid(True)
-    plt.plot(y,vetor, 'ro')
+    plt.plot(x,vetor, 'ro')
     plt.show()
     
 
 
-def bubble_sort():
-    return 
-
+def bubble_sort(vetor, tamanho, x):
+   
+    plt.show()
+    trocou = True
+    while trocou:
+        plt.title("Bubble Sort")
+        plt.grid(True)
+        plt.plot(x, vetor, 'ro')
+        plt.pause(0.001)
+        plt.clf()
+        trocou = False
+        for i in range(1, tamanho):
+            if vetor[i]<vetor[i-1]:
+                aux = vetor[i-1]
+                vetor[i-1] = vetor[i]
+                vetor[i] = aux
+                trocou = True
+    plt.title("Bubble Sort")
+    plt.grid(True)
+    plt.plot(x, vetor, 'ro')
+    plt.show()
+    
 
 vetor = [] 
 tamanho = 0 
-y=[]
+x=[]
 
 #cria vetor
-for i in range(0,300):
-   vetor.append(random.randint(-20,350)) 
-   y.append(tamanho+1)
+for i in range(0,350):
+   vetor.append(random.randint(-200,350)) 
+   x.append(tamanho+1)
    tamanho = tamanho + 1
 
-#insertion_sort(vetor, tamanho, y)
-selection_sort(vetor, tamanho, y)
+#insertion_sort(vetor, tamanho, x)
+selection_sort(vetor, tamanho, x)
+
+#bubble_sort(vetor, tamanho,x)
